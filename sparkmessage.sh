bed1374d3d58cf9b1a52be73e38d400759045ed0
patglanz <pat.glanz@googlemail.com>
2016-06-24 13:01:27 +0200

    Create sparkmessage.sh

---
+++ sparkmessage.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+accesstoken="your_spark_access_token_here" roomId="the_room_id_of_the_room_you_want_domoticz_to_post_messages_to"
+curl https://api.ciscospark.com/v1/messages -X POST -H "Authorization:Bearer $accesstoken" --data "roomId=$roomId" --data "text=$1"
